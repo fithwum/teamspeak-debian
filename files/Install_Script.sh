2bd426f39f4e94c63610c5540e74360773748f94
fithwum <34775371+fithwum@users.noreply.github.com>
2018-12-30 12:41:58 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -6,8 +6,6 @@
 TS_VERSION="3.5.1"
 CHANGELOG=/ts3server/CHANGELOG_${TS_VERSION}
 
-apt-get install -y wget
-
 # Main Install.
 if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 	then
@@ -24,6 +22,7 @@ if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 			echo "INFO ! ts3server is ${TS_VERSION} ... checking ini/sh files before running current docker."
 		else
 			echo "WARNING ! ts3server is out of date ... will download new copy from teamspeak."
+				apt-get install -y wget
 				wget https://files.teamspeak-services.com/releases/server/${TS_VERSION}/teamspeak3-server_linux_amd64-${TS_VERSION}.tar.bz2 -O /ts3temp/ts3server_${TS_VERSION}.tar.bz2
 				sleep 2
 				tar -xf /ts3temp/ts3server_${TS_VERSION}.tar.bz2 -C /ts3temp/serverfiles --strip-components=1
