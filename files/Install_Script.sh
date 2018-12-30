068b1ab73051f2b85370e42b1d043966adc3501c
fithwum <34775371+fithwum@users.noreply.github.com>
2018-12-30 12:58:57 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -6,6 +6,37 @@
 TS_VERSION="3.5.1"
 CHANGELOG=/ts3server/CHANGELOG_${TS_VERSION}
 
+function __wget() {
+    : ${DEBUG:=0}
+    local URL=$1
+    local tag="Connection: close"
+    local mark=0
+
+    if [ -z "${URL}" ]; then
+        printf "Usage: %s \"URL\" [e.g.: %s http://www.google.com/]" \
+               "${FUNCNAME[0]}" "${FUNCNAME[0]}"
+        return 1;
+    fi
+    read proto server path <<<$(echo ${URL//// })
+    DOC=/${path// //}
+    HOST=${server//:*}
+    PORT=${server//*:}
+    [[ x"${HOST}" == x"${PORT}" ]] && PORT=80
+    [[ $DEBUG -eq 1 ]] && echo "HOST=$HOST"
+    [[ $DEBUG -eq 1 ]] && echo "PORT=$PORT"
+    [[ $DEBUG -eq 1 ]] && echo "DOC =$DOC"
+
+    exec 3<>/dev/tcp/${HOST}/$PORT
+    echo -en "GET ${DOC} HTTP/1.1\r\nHost: ${HOST}\r\n${tag}\r\n\r\n" >&3
+    while read line; do
+        [[ $mark -eq 1 ]] && echo $line
+        if [[ "${line}" =~ "${tag}" ]]; then
+            mark=1
+        fi
+    done <&3
+    exec 3>&-
+}
+
 # Main Install.
 if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 	then
@@ -23,7 +54,7 @@ if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 		else
 			echo "WARNING ! ts3server is out of date ... will download new copy from teamspeak."
 				apt-get install -y wget
-				wget https://files.teamspeak-services.com/releases/server/${TS_VERSION}/teamspeak3-server_linux_amd64-${TS_VERSION}.tar.bz2 -O /ts3temp/ts3server_${TS_VERSION}.tar.bz2
+				__wget https://files.teamspeak-services.com/releases/server/${TS_VERSION}/teamspeak3-server_linux_amd64-${TS_VERSION}.tar.bz2 -O /ts3temp/ts3server_${TS_VERSION}.tar.bz2
 				sleep 2
 				tar -xf /ts3temp/ts3server_${TS_VERSION}.tar.bz2 -C /ts3temp/serverfiles --strip-components=1
 				sleep 2
@@ -42,7 +73,7 @@ if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 			echo "INFO ! ts3server_minimal_runscript.sh found ... will not download."
 		else
 			echo "WARNING ! ts3server_minimal_runscript.sh not found ... will download new copy."
-				wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/scripts/ts3server_minimal_runscript.sh -O /ts3temp/inifiles/ts3server_minimal_runscript.sh
+				__wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/scripts/ts3server_minimal_runscript.sh -O /ts3temp/inifiles/ts3server_minimal_runscript.sh
 				cp /ts3temp/inifiles/ts3server_minimal_runscript.sh /ts3server/
 				rm -frv /ts3temp/ts3server_minimal_runscript.sh
 	fi
@@ -51,7 +82,7 @@ if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 			echo "INFO ! ts3db_mariadb.ini found ... will not download."
 		else
 			echo "WARNING ! ts3db_mariadb.ini not found ... will download new copy."
-				wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/files/ts3db_mariadb.ini -O /ts3temp/inifiles/ts3db_mariadb.ini
+				__wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/files/ts3db_mariadb.ini -O /ts3temp/inifiles/ts3db_mariadb.ini
 				cp /ts3temp/inifiles/ts3db_mariadb.ini /ts3server/
 				rm -frv /ts3temp/inifiles/ts3db_mariadb.ini
 	fi
@@ -60,7 +91,7 @@ if [ "/ts3server/CHANGELOG_*" == "/ts3server/CHANGELOG_${TS_VERSION}" ]
 			echo "INFO ! ts3server.ini found ... will not download."
 		else
 			echo "WARNING ! ts3server.ini not found ... will download new copy."
-				wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/files/ts3server.ini -O /ts3temp/inifiles/ts3server.ini
+				__wget https://raw.githubusercontent.com/fithwum/files-for-dockers/master/files/ts3server.ini -O /ts3temp/inifiles/ts3server.ini
 				cp /ts3temp/inifiles/ts3server.ini /ts3server/
 				rm -frv /ts3temp/inifiles/ts3server.ini
 	fi
