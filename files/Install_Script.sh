40973627090b696935484bad75299cb21d49b04b
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-02 15:19:19 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -15,6 +15,7 @@ if [ -e "${CHANGELOG}" ]
 		echo "WARNING ! ts3server is out of date ... will download new copy from teamspeak."
 			sleep 1
 			echo "Clearing old teamspeak files and preserving settings/logs/userfiles."
+			echo "(this will take some time if you have uploaded many/large files)"
 			cp -R /ts3server/files/. /ts3temp/serverfiles/files/
 			cp -R /ts3server/logs/. /ts3temp/serverfiles/logs/
 			cp -v /ts3server/*.ini /ts3temp/serverfiles
