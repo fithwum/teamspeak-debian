07a32f4802a4750d7f0cd902c2e012ea523a5786
fithwum <34775371+fithwum@users.noreply.github.com>
2019-01-03 11:57:38 -0800

    Update Install_Script.sh

--- files/Install_Script.sh
+++ files/Install_Script.sh
@@ -3,14 +3,19 @@
 # All rights reserved
 
 # Variables.
-TS_VERSION="3.5.1"
+echo "Checking server version from teamspeak."
+wget --no-cache https://www.teamspeak.com/versions/server.json -O /ts3temp/server.json
+TS_VERSION_CHECK=$(cat /ts3temp/server.json | grep version | head -1 | awk -F: '{print $4}' | sed 's/[",]//g' | sed "s/checksum//g")
+TS_VERSION=${TS_VERSION_CHECK}
 CHANGELOG=/ts3server/CHANGELOG_${TS_VERSION}
+echo "Latest server version from teamspeak:$TS_VERSION"
 
 # Main install (debian).
 # Check for files in /ts3server and download/create if needed.
 if [ -e "${CHANGELOG}" ]
 	then
 		echo "INFO ! ts3server is ${TS_VERSION} ... checking that ini/sh files exist before running current docker."
+		rm -fr /ts3temp/server.json
 	else
 		echo "WARNING ! ts3server is out of date ... will download new copy from teamspeak."
 			sleep 1
@@ -18,8 +23,8 @@ if [ -e "${CHANGELOG}" ]
 			echo "(this will take some time if you have uploaded many/large files)"
 			cp -R /ts3server/files/. /ts3temp/serverfiles/files/
 			cp -R /ts3server/logs/. /ts3temp/serverfiles/logs/
-			cp -v /ts3server/*.ini /ts3temp/serverfiles
-			cp -v /ts3server/*.sh /ts3temp/serverfiles
+			cp /ts3server/*.ini /ts3temp/serverfiles
+			cp /ts3server/*.sh /ts3temp/serverfiles
 			rm -fr /ts3server/*
 			cp -R /ts3temp/serverfiles/. /ts3server/
 			rm -fr /ts3temp/serverfiles/*
@@ -28,13 +33,14 @@ if [ -e "${CHANGELOG}" ]
 			sleep 1
 			tar -xf /ts3temp/ts3server_${TS_VERSION}.tar.bz2 -C /ts3temp/serverfiles --strip-components=1
 			sleep 1
-			rm -frv /ts3temp/serverfiles/ts3server_startscript.sh
-			rm -frv /ts3temp/ts3server_${TS_VERSION}.tar.bz2
+			rm -fr /ts3temp/serverfiles/ts3server_startscript.sh
+			rm -fr /ts3temp/ts3server_${TS_VERSION}.tar.bz2
 			cp -uR /ts3temp/serverfiles/. /ts3server/
 			sleep 1
 			mv /ts3server/redist/libmariadb.so.2 /ts3server/libmariadb.so.2
 			mv /ts3server/CHANGELOG ${CHANGELOG}
 			rm -fr /ts3temp/serverfiles/*
+			rm -fr /ts3temp/server.json
 fi
 
 # Check if the ini/sh files exist in /ts3server and download/create if needed.
@@ -45,7 +51,7 @@ if [ -e /ts3server/ts3server_minimal_runscript.sh ]
 		echo "WARNING ! ts3server_minimal_runscript.sh not found ... will download new copy."
 			wget --no-cache https://raw.githubusercontent.com/fithwum/files-for-dockers/master/scripts/ts3server_minimal_runscript.sh -O /ts3temp/inifiles/ts3server_minimal_runscript.sh
 			cp /ts3temp/inifiles/ts3server_minimal_runscript.sh /ts3server/
-			rm -frv /ts3temp/ts3server_minimal_runscript.sh
+			rm -fr /ts3temp/ts3server_minimal_runscript.sh
 fi
 if [ -e /ts3server/ts3db_mariadb.ini ]
 	then
@@ -54,7 +60,7 @@ if [ -e /ts3server/ts3db_mariadb.ini ]
 		echo "WARNING ! ts3db_mariadb.ini not found ... will download new copy."
 			wget --no-cache https://raw.githubusercontent.com/fithwum/files-for-dockers/master/files/ts3db_mariadb.ini -O /ts3temp/inifiles/ts3db_mariadb.ini
 			cp /ts3temp/inifiles/ts3db_mariadb.ini /ts3server/
-			rm -frv /ts3temp/inifiles/ts3db_mariadb.ini
+			rm -fr /ts3temp/inifiles/ts3db_mariadb.ini
 fi
 if [ -e /ts3server/ts3server.ini ]
 	then
@@ -63,7 +69,7 @@ if [ -e /ts3server/ts3server.ini ]
 		echo "WARNING ! ts3server.ini not found ... will download new copy."
 			wget --no-cache https://raw.githubusercontent.com/fithwum/files-for-dockers/master/files/ts3server.ini -O /ts3temp/inifiles/ts3server.ini
 			cp /ts3temp/inifiles/ts3server.ini /ts3server/
-			rm -frv /ts3temp/inifiles/ts3server.ini
+			rm -fr /ts3temp/inifiles/ts3server.ini
 fi
 
 sleep 1
@@ -71,8 +77,8 @@ sleep 1
 # Set permissions.
 chown 99:100 -R /ts3server
 chmod 777 -R /ts3server
-chmod +x -v /ts3server/ts3server_minimal_runscript.sh
-chmod +x -v /ts3server/ts3server
+chmod +x /ts3server/ts3server_minimal_runscript.sh
+chmod +x /ts3server/ts3server
 sleep 1
 
 # Run teamspeak server.
